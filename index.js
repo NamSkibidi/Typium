7175053cb5ccca558435b86aa1ca0e58861e99c3
root <root@localhost.localdomain>
2025-06-22 23:49:05 +0000

    feat: sync

---
+++ index.js
@@ -0,0 +1,19 @@
+const Typium = new (class {
+  #data;
+  add(obj) {
+
+    const isNameHasSpace
+    if (obj.name) {
+      if (obj.value) { }
+      if (obj.type) {
+        if (obj.value instanceof obj.type) {
+
+        }
+      }
+    }
+  }
+}
+  get _() {
+  return this.#data;
+}
+}) ();
