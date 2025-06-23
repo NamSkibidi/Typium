ea317c95ad0ff8869c8fe8af1093b81d289df6eb
root <root@localhost.localdomain>
2025-06-23 23:34:07 +0000

    1.1.1

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Typium
+
+A small library for people who want a simple type safe for variables.
+Here is how to use:
+```JavaScript
+const Typium = require("typium");
+Typium.add({
+  name: "A", // Required, must be string.
+  type: String, // Class name.
+  value: "Hello World!" // Set the value.
+})
+// Now that you has succcessfully create a value now let's get it.
+let value = Typium.get("A");
+console.log(value); // "Hello World!"
+// But what if I gave it the wrong type?
+// It'll do nothing.
+// So how do I change the value?
+// There is 2 way:
+// 1:
+Typium.set("A", "Lorem Ipsum");
+console.log(Typium.get("A"); // "Lorem Ipsum"
+// 2:
+Typium.add({
+  name: "A",
+  value: "dolor amet",
+  overrideValue: true, 
+  type: Window // Type will be ignored if `overrideType` enable, but you still need to add it as any class or it won't work.
+});
+console.log(Typium.get("A"); // "dolor amet"
+// What about changing the type?
+// Use `overrideType` (boolean) in Typium.add() along with `overrideValue` and `value` having the exact type, this will make  overrideValue do not ignore `type`.
+```
+
+> I made this for myself but I wanna publish it for people who in need. I only update this whenever I want to so don't expect me to update constantly.
--- index.js
+++ index.js
@@ -1,19 +1,67 @@
 const Typium = new (class {
-  #data;
+  #data = {};
+
   add(obj) {
+    if (
+      typeof obj.name !== "string" ||
+      obj.value === undefined ||
+      !obj.type
+    ) return;
 
-    const isNameHasSpace
-    if (obj.name) {
-      if (obj.value) { }
-      if (obj.type) {
-        if (obj.value instanceof obj.type) {
+    const exists = this.#data[obj.name];
 
+    if (!exists) {
+      this.#data[obj.name] = { value: obj.value, type: obj.type };
+    } else {
+      if (obj.overrideValue === 1) {
+        if (obj.overrideType === 1) {
+          if (obj.value instanceof obj.type) {
+            this.#data[obj.name] = { value: obj.value, type: obj.type };
+          } else {
+            this.#data[obj.name] = {
+              value: this.#wrapValue(obj.value, obj.type),
+              type: obj.type
+            };
+          }
+        } else {
+          if (obj.value instanceof exists.type) {
+            this.#data[obj.name].value = obj.value;
+          } else {
+            this.#data[obj.name].value = this.#wrapValue(obj.value, exists.type);
+          }
         }
       }
     }
   }
-}
-  get _() {
-  return this.#data;
-}
-}) ();
+
+  #wrapValue(value, type) {
+    if (type === String) return new String(value);
+    if (type === Number) return new Number(value);
+    if (type === Boolean) return new Boolean(value);
+    return value;
+  }
+
+  get(name) {
+    const entry = this.#data[name];
+    if (!entry) return undefined;
+
+    const { value, type } = entry;
+    if ([String, Number, Boolean].includes(type)) {
+      return value.valueOf();
+    }
+    return value;
+  }
+
+  set(name, val) {
+    const entry = this.#data[name];
+    if (!entry) return;
+
+    const { type } = entry;
+
+    if (val instanceof type) {
+      entry.value = val;
+    } else {
+      entry.value = this.#wrapValue(val, type);
+    }
+  }
+})();
